fd3b022cc5944e865f02bd4419f0399f7bf5df10
Nikolas <michalickanikolas@gmail.com>
2021-12-12 00:28:12 +0100

    vlastna funkcia na citanie zo suboru

--- main.cpp
+++ main.cpp
@@ -6,73 +6,49 @@
 
 using namespace std;
 
+string *ReadFile(string fileName){
+    ifstream myFileL(fileName);
+    ifstream myFile(fileName);
+    string myText;
+    int numberOfLines = 0;
+
+    if (myFileL.is_open()){
+        while (getline(myFileL,myText)){
+            numberOfLines++;
+        }
+        //cout << "Number of lines: " << numberOfLines << endl;
+        myFileL.close();
+    }
+
+    string * PoleRead = new string[numberOfLines];
+    
+    if (myFile.is_open()){
+        int i = 0;
+        while (getline(myFile,myText)){
+            PoleRead[i] = myText;
+            i++;
+        }
+        myFile.close();
+    }
+    else cout << "Unable to open file"; 
+
+
+    return PoleRead;
+}
+
 int main(){
     srand(time(NULL));
     system("CLS");
-
-    ifstream MyFileOtazkyC("otazky.txt");
-    ifstream MyFileOtazky("otazky.txt");
-    ifstream MyFileOdpovedeC("odpovede.txt");
-    ifstream MyFileOdpovede("odpovede.txt");
-    string myText;
-    int numberOfLinesOtazky = 0;
-    int numberOfLinesOdpovede = 0;
-
-    // get number of lines otazky
-    if (MyFileOtazkyC.is_open()){
-        while (getline(MyFileOtazkyC,myText)){
-            numberOfLinesOtazky++;
-        }
-        cout << "Number of lines: " << numberOfLinesOtazky << endl;
-        MyFileOtazkyC.close();
-    }
-
-    // save lines from file otazky to array
-    string PoleOtazky[numberOfLinesOtazky];
-    if (MyFileOtazky.is_open()){
-        int i = 0;
-        while (getline(MyFileOtazky,myText)){
-            PoleOtazky[i] = myText;
-            i++;
-        }
-        MyFileOtazky.close();
-    }
-    else cout << "Unable to open file"; 
-
-
-
-
-
-    // get number of lines odpovede
-    if (MyFileOdpovedeC.is_open()){
-        while (getline(MyFileOdpovedeC,myText)){
-            numberOfLinesOdpovede++;
-        }
-        cout << "Number of lines: " << numberOfLinesOdpovede << endl;
-        MyFileOdpovedeC.close();
-    }
-
-    // save lines from file odpovede to array
-    string PoleOdpovede[numberOfLinesOdpovede];
-    if (MyFileOdpovede.is_open()){
-        int i = 0;
-        while (getline(MyFileOdpovede,myText)){
-            PoleOdpovede[i] = myText;
-            i++;
-        }
-        MyFileOdpovede.close();
-    }
-    else cout << "Unable to open file"; 
-
     
+    string * PoleOtazky;
+    PoleOtazky = ReadFile("otazky.txt");
+    string * PoleOdpovede;
+    PoleOdpovede = ReadFile("odpovede.txt");
 
-    // printing to terminal some debug information
-    for(int i=0;i<numberOfLinesOtazky;i++){
+    for(int i=0;i<2;i++){
         cout << PoleOtazky[i] << '\n';
     }
-    for(int i=0;i<numberOfLinesOdpovede;i++){
-        cout << PoleOdpovede[i] << '\n';
-    }
-
+    
+    //system("PAUSE");
     return 0;
 }
